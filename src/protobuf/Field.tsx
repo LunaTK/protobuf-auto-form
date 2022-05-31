0fa8bd42d6ca178552afc2cf966840fca0214c7b
Taegeun Moon <taegeun.moon@devsisters.com>
2022-05-31 12:56:58 +0900

    refactor: make Field universal

--- src/protobuf/Field.tsx
+++ src/protobuf/Field.tsx
@@ -1,23 +1,45 @@
 import React from 'react';
+import protobuf from 'protobufjs';
+import OneofInput from './input/Oneof';
+import Input from './Input';
 
 interface Props {
+  field: protobuf.Field | protobuf.OneOf
   name: string
-  type: string
 }
 
-const Field: React.FC<Props> = ({ name, type, children }) => (
-  <>
+const Label: React.FC<{
+  field: Props['field']
+}> = ({ field }) => {
+  const isOneof = field instanceof protobuf.OneOf;
+
+  return (
     <span className="text-right inline-flex flex-col">
       <span className="leading-3 font-bold">
-        {name}
+        {field.name}
       </span>
       <span className="text-slate-500 text-sm">
         (
-        {type}
+        {isOneof ? 'oneof' : field.type}
         )
       </span>
     </span>
-    {children}
+  );
+};
+
+const Field: React.FC<Props> = ({ field, name }) => (
+  <>
+    <Label field={field} />
+    {field instanceof protobuf.OneOf
+      ? <OneofInput oneof={field} />
+      : (
+        <Input
+          name={`${name}.${field.name}`}
+          repeated={field.repeated}
+          resolvedType={field.resolvedType}
+          type={field.type}
+        />
+      )}
   </>
 );
 
--- src/protobuf/input/Message.tsx
+++ src/protobuf/input/Message.tsx
@@ -1,8 +1,6 @@
 import React, { useMemo } from 'react';
 import protobuf from 'protobufjs';
 import Field from '../Field';
-import OneofInput from './Oneof';
-import Input from '../Input';
 
 interface Props {
   type: protobuf.Type
@@ -19,7 +17,7 @@ const useMessage = (type: protobuf.Type) => {
       (field) => !everyOneofFields.has(field.name),
     );
 
-    const oneofs = type.oneofs === undefined ? [] : Object.values(type.oneofs);
+    const oneofs = Object.values(type.oneofs ?? {});
 
     const hasOneAndOnlyField = everyOneofFields.size === 0
       ? fields.length === 1
@@ -40,26 +38,15 @@ const Message: React.FC<Props> = ({ type, name = '' }) => {
 
   return (
     <div className="grid grid-cols-2 gap-4">
-      {fields.map((field) => (
-        <Field
-          name={`${name}.${field.name}`}
-          type={field.type}
-          key={field.name}
-        >
-          <Input
+      {
+        [...fields, ...oneofs].map((field) => (
+          <Field
             name={`${name}.${field.name}`}
-            repeated={field.repeated}
-            resolvedType={field.resolvedType}
-            type={field.type}
+            field={field}
+            key={field.name}
           />
-        </Field>
-      ))}
-      {oneofs.map((oneof) => (
-        <OneofInput
-          key={oneof.name}
-          oneof={oneof}
-        />
-      ))}
+        ))
+      }
     </div>
   );
 };
