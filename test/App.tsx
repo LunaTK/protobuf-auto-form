4348063c2210466be532d9044c7bfdbcfff29dab
LunaTK <ahn_v3@naver.com>
2022-06-01 21:51:49 +0900

    chore: add initialState example

--- test/App.tsx
+++ test/App.tsx
@@ -2,6 +2,20 @@ import React from 'react';
 import descriptor from './proto.json';
 import AutoForm from '../src/AutoForm';
 
+const initialState = {
+  title: 'Title example',
+  type: '0',
+  tags: ['tag 1', 'tag 2'],
+  referrers: {
+    user1: '12',
+    user2: '33',
+  },
+  comments: [],
+  content: 'this is a content',
+  author: 'userId',
+  userId: '7748',
+};
+
 const App = () => (
   <div>
     <AutoForm
@@ -11,6 +25,7 @@ const App = () => (
       onSubmitValid={(values) => {
         console.log(values);
       }}
+      initialState={initialState}
     >
       <button type="submit" className="btn btn-xs btn-accent">Submit</button>
     </AutoForm>
