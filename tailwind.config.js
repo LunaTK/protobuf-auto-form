ddee9b838a66a7dc5886d7d942fdd23fecaa6507
LunaTK <ahn_v3@naver.com>
2022-06-01 09:15:42 +0900

    feat: setup library bundling

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,7 @@ module.exports = {
     extend: {},
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: false,
+  },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxRuntime: 'classic',
-  })],
+  plugins: [
+    react({
+      jsxRuntime: 'classic',
+    }),
+    dts({ insertTypesEntry: true }),
+    visualizer(),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'AutoForm',
+      formats: ['es'],
+      fileName: (format) => `AutoForm.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-hook-form', 'protobufjs'],
+    },
+    sourcemap: true,
+  },
 });
