c15bd29a1b60a5819f993c12e46cf2b94a68205c
Taegeun Moon <taegeun.moon@devsisters.com>
2022-06-03 10:53:43 +0900

    bug: fix RadioButton htmlFor

--- src/common/RadioButton.tsx
+++ src/common/RadioButton.tsx
@@ -12,10 +12,11 @@ const RadioButton: React.FC<Props> = ({
   value, name, label = value, defaultChecked = false,
 }) => {
   const { register } = useFormContext();
+  const id = `${name}.${value}`;
 
   return (
-    <label htmlFor={label} className="cursor-pointer inline-flex items-center justify-start space-x-2">
-      <input type="radio" className="radio-xs" id={label} value={value} defaultChecked={defaultChecked} {...register(name)} />
+    <label htmlFor={id} className="cursor-pointer inline-flex items-center justify-start space-x-2">
+      <input type="radio" className="radio-xs" id={id} value={value} defaultChecked={defaultChecked} {...register(name)} />
       <span className="label-text">
         {label}
       </span>
