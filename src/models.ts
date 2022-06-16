d8cf0630680f1a914283772c611e16c39dc47c96
Taegeun Moon <taegeun.moon@devsisters.com>
2022-06-16 19:51:12 +0900

    fix overridden props

--- src/models.ts
+++ src/models.ts
@@ -2,8 +2,8 @@ import { UseFormWatch } from 'react-hook-form';
 
 export interface OverriddenFieldProps<T = any> {
   watch: UseFormWatch<T>
-  value: T
-  onChange: (newValue: T) => void
+  value?: T | null
+  onChange?: (newValue?: T) => void
   /**
    * Index of repeated or map field
    */
