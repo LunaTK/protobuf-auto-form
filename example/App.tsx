a622a88d300396525e9f7dbab9d8fecf3a724103
Taegeun Moon <taegeun.moon@devsisters.com>
2022-06-16 19:51:21 +0900

    update App.tsx

--- example/App.tsx
+++ example/App.tsx
@@ -2,21 +2,36 @@ import React from 'react';
 import protobuf from 'protobufjs';
 import descriptor from './proto.json';
 import AutoForm from '../src/AutoForm';
+import { OverriddenFieldProps } from '../src/models';
 
 const namespace = protobuf.Namespace.fromJSON('', descriptor);
 
 const { Field } = AutoForm;
 
+const Comment: React.VFC<OverriddenFieldProps<{
+  author: string
+  content: string
+
+  _something: 's1' | 'type'
+  s1?: string
+  type?: 'SIMPLE' | 'DETAILED'
+}>> = ({ value }) => (
+  <div>
+    <div>{value.author}</div>
+    <div>{value.content}</div>
+  </div>
+);
+
 const App = () => (
   <div>
     <AutoForm
-      messageType="Guild"
+      messageType="Article"
       namespace={namespace}
       onSubmitValid={(values) => {
         console.log(values);
       }}
     >
-      {/* <Field name="title" label="타이틀" disabled />
+      <Field name="title" label="타이틀" disabled />
       <Field name="tags" hidden />
 
       <Field name="referrers" label="참조">
@@ -24,22 +39,22 @@ const App = () => (
         <Field name="value" label="횟수" />
       </Field>
 
-      <Field name="comments" label="댓글">
-        <Field name="author" label="글쓴이" disabled />
+      <Field name="comments" label="댓글" prepend={<b>오우예압</b>}>
+        <Field name="value" render={Comment} />
       </Field>
 
       <Field name="author" label="글쓴이">
-        <Field name="nickname" label="닉네임" />
-      </Field> */}
+        <Field name="nickname" label="닉네임" render={() => <div>hi</div>} />
+      </Field>
 
-      <Field name="members">
+      {/* <Field name="members">
         <Field name="value">
           <Field name="userId" disabled label="유저ID" />
           <Field name="friends" label="친구목록">
             <Field name="value" disabled />
           </Field>
         </Field>
-      </Field>
+      </Field> */}
 
       <button type="submit" className="btn btn-xs btn-accent">Submit</button>
     </AutoForm>
