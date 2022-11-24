5aa1a67d26bd412e51270d87faf2939b2ab4131e
Taegeun Moon <taegeun.moon@devsisters.com>
2022-11-24 10:39:07 +0900

    add

--- src/protobuf/input/arrayLike/ArrayLike.tsx
+++ src/protobuf/input/arrayLike/ArrayLike.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import AddButton from '../../../common/AddButton';
 import DelButton from '../../../common/DelButton';
 
@@ -9,20 +9,35 @@ interface Props {
   fields: Record<'id', string>[];
 }
 
+const collapseIfTooMany = (nodes: ReactNode[]) => {
+  if (nodes.length < 10) return nodes;
+
+  return (
+    <details className="open">
+      <summary className="cursor-pointer text-gray-600">Show items</summary>
+      {nodes}
+    </details>
+  );
+};
+
 const ArrayLike: React.VFC<Props> = ({ fields, onAdd, onRemove, render }) => {
   return (
     <div>
-      <header className="flex items-center gap-2 text-gray-400">
+      <span className="flex items-center gap-2 text- mb-2">
         <AddButton onClick={onAdd} />
         ({fields.length} items)
-      </header>
-
-      {fields.map((f, idx) => (
-        <div key={f.id} className="flex items-center gap-2 my-2">
-          <DelButton onClick={() => onRemove(idx)} />
-          {render({ idx })}
-        </div>
-      ))}
+      </span>
+      {collapseIfTooMany(
+        fields.map((f, idx) => (
+          <div key={f.id} className="flex items-center gap-2 my-2">
+            <span className="w-min text-center text-gray-400">
+              {idx}
+              <DelButton onClick={() => onRemove(idx)} />
+            </span>
+            {render({ idx })}
+          </div>
+        )),
+      )}
     </div>
   );
 };
